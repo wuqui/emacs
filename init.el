5e3c8461db699048411678772b9da66467b24e1d
wuqui <q.wuerschinger@gmail.com>
2021-06-09 11:13:41 +0200

    activate `gnuplot` module

--- init.el
+++ init.el
@@ -154,6 +154,7 @@
         +noter
         +jupyter
         +present
+        +gnuplot
         )               ; organize your plain life in plain text
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
