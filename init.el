81e4606526abbee0759c21b3118b33098d0cf0f1
wuqui <q.wuerschinger@gmail.com>
2021-10-31 16:25:55 +0100

    deactivate `biblio` module
    > custom cfg in `config.org`

--- init.el
+++ init.el
@@ -87,7 +87,7 @@
 
        :tools
        ;;ansible
-       biblio
+       ;; biblio
        ;;debugger          ; FIXME stepping through code, to help you add bugs
        ;;direnv
        ;;docker
