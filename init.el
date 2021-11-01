e165d70495565f7411b97f24c56991426242e050
wuqui <q.wuerschinger@gmail.com>
2021-11-01 15:13:53 +0100

    deactivate `present` module

--- init.el
+++ init.el
@@ -163,6 +163,7 @@
         +noter
         +present
         +pretty
+        ;; +present
         )               ; organize your plain life in plain text
        ;;php               ; perl's insecure younger brother
        ;;plantuml          ; diagrams for confusing people more
