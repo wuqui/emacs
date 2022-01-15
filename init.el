0280a3ea5ded42416f853bae69d988028a1bac9f
wuqui <q.wuerschinger@gmail.com>
2022-01-15 14:48:57 +0100

    add icons to `vertico`

--- init.el
+++ init.el
@@ -24,7 +24,9 @@
        ;;helm              ; the *other* search engine for love and life
        ;;ido               ; the other *other* search engine...
        ;;ivy               ; a search engine for love and life
-       vertico           ; the search engine of the future
+       (vertico
+        +icons
+        )           ; the search engine of the future
 
        :ui
        deft              ; notational velocity for Emacs
